e1fa2f2b98810d8a1f98afb14b797b11d8013bf3
Nate De Luna <nate.d.luna@gmail.com>
2024-02-28 14:13:13 -0600

    Update package dependencies

---
+++ system_check.py
@@ -0,0 +1,10 @@
+class ElementSelectorsCheck:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def check(self):
+        try:
+            pass
+        except Exception as e:
+            return False
+        return True
